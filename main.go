e96287bfe5c93b874b463678a4544893d1dfd526
Jonathan Cua <jonathan.cua@gmail.com>
2016-04-09 08:27:51 -0700

    add main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/vault-cli/commands"
+)
+
+const Name = "vault-cli"
+const Version = "0.1.0"
+
+func main() {
+	log.SetOutput(ioutil.Discard)
+
+	root := commands.Init(Name, Version)
+	if err := root.Execute(); err != nil {
+		fmt.Fprintln(root.Err, err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
