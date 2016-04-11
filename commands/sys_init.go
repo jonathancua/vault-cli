8496e39ec1ddea554abcac8877c1046311dc7551
jonathancua <jonathan.cua@gmail.com>
2016-04-11 15:47:00 -0700

    sys has a lot of subcommands. Remove sys as parent command, and make the subcommands as parent commands instead.

---
+++ commands/sys_init.go
@@ -0,0 +1,36 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func (s *Sys) AddInitStatusSub(c *cobra.Command) {
+	lookupCmd := &cobra.Command{
+		Use:   "initStatus",
+		Short: "Return the initialization status of a Vault.",
+		Long:  "Return the initialization status of a Vault.",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return s.InitStatus(args)
+		},
+	}
+
+	s.AddCommand(lookupCmd)
+}
+
+func (s *Sys) InitStatus(args []string) error {
+	sys, err := s.Sys()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := sys.InitStatus()
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+
+	return s.Output(result)
+}
